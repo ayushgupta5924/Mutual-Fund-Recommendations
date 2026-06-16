43cd239882002cfc0afa545032c50a26e95e6274
Ayush Gupta <136419465+ayushgupta5924@users.noreply.github.com>
2026-06-16 11:08:32 +0530

    Update constants.dart

--- flutter_app/mf_advisor/lib/utils/constants.dart
+++ flutter_app/mf_advisor/lib/utils/constants.dart
@@ -4,8 +4,8 @@ class AppConstants {
   static const String appName = 'MF Advisor';
   static const String appTagline = 'Smart Mutual Fund Recommendations';
   
-  static const String baseUrl = kReleaseMode 
-      ? 'https://mf-advisor-api.onrender.com/api' 
+  static const String baseUrl = kReleaseMode
+      ? 'https://mfrecommender.onrender.com/api' 
       : 'http://localhost:8000/api';
   
   static const String moneyControlBaseUrl = 'https://www.moneycontrol.com/mutual-funds/nav/hdfc-mutual-fund';
